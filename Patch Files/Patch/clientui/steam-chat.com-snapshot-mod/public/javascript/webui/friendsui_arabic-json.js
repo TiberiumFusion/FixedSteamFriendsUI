b06d6796c0e6df785140fb519a446eca8c241e39
TiberiumFusion <6332277+TiberiumFusion@users.noreply.github.com>
2024-08-14 18:17:13 -0400

    Patch files: bring in patched 9097133 snapshot

--- Patch Files/Patch/clientui/steam-chat.com-snapshot-mod/public/javascript/webui/friendsui_arabic-json.js
+++ Patch Files/Patch/clientui/steam-chat.com-snapshot-mod/public/javascript/webui/friendsui_arabic-json.js
@@ -11,4 +11,4 @@
 
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/. 
 ****/
-"use strict";(self.webpackChunk_steam_friendsui=self.webpackChunk_steam_friendsui||[]).push([[6992],{58480:e=>{e.exports=JSON.parse('{"language":"arabic"}')}}]);
+"use strict";(self.webpackChunk_steam_friendsui=self.webpackChunk_steam_friendsui||[]).push([[6518],{83784:e=>{e.exports=JSON.parse('{"language":"arabic"}')}}]);
--- Patch Files/Patch/clientui/steam-chat.com-snapshot-mod/public/javascript/webui/shared_arabic-json.js
+++ Patch Files/Patch/clientui/steam-chat.com-snapshot-mod/public/javascript/webui/shared_arabic-json.js
@@ -11,4 +11,4 @@
 
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/. 
 ****/
-"use strict";(self.webpackChunk_steam_friendsui=self.webpackChunk_steam_friendsui||[]).push([[2431],{78501:e=>{e.exports=JSON.parse('{"language":"arabic"}')}}]);
+"use strict";(self.webpackChunk_steam_friendsui=self.webpackChunk_steam_friendsui||[]).push([[3710],{23287:e=>{e.exports=JSON.parse('{"language":"arabic"}')}}]);
